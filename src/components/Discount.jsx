de0e5924771504f8a1ffafb2bbebaa3a62d1e236
alpertann <squelchh8@gmail.com>
2024-01-06 15:04:15 +0300

    first

---
+++ src/components/Discount.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Discount() {
+  return (
+    <div className="discount">
+        <h3>a</h3>
+    </div>
+  )
+}
+
+export default Discount
---
+++ src/components/main.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <div className="hero border-1 pb-3">
+        <div className="card">
+          <img
+            className="card-img img-fluid"
+            src="./assets/main.png.jpg"
+            alt="Card"
+            height={600}
+          />
+          <div className="card-img-overlay d-flex align-items-center">
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
